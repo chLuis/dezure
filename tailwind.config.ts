38f618e0455097a0b4416cd94919b544f7fb08e8
Luis Chrestia <chrestia.luis@gmail.com>
2024-05-10 11:14:25 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "showSlow" : "showSlow 0.2s ease-in-out",
+        "showFromBottom" : "showFromBottom 0.3s ease-in-out"
+      },
+      keyframes: {
+        showSlow: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        showFromBottom: {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      }
     },
   },
   plugins: [],
